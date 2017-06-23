5120d110c984d497b0d50be06e9ea0efd9a855ca
Krishnaswamy Subramanian <subramk@thoughtworks.com>
2017-06-23 09:27:08 +0530

    Initial Commit

---
+++ SwiftBasic/SwiftBasic.h
@@ -0,0 +1,19 @@
+//
+//  SwiftBasic.h
+//  SwiftBasic
+//
+//  Created by Krishnaswamy S on 23/06/17.
+//  Copyright © 2017 Ayuda Heuristics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftBasic.
+FOUNDATION_EXPORT double SwiftBasicVersionNumber;
+
+//! Project version string for SwiftBasic.
+FOUNDATION_EXPORT const unsigned char SwiftBasicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftBasic/PublicHeader.h>
+
+
